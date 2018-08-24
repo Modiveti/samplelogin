a1d23a62575f328f07ae1be53e3bbadb4eed3f10
Odiveti, Maheswara Reddy <odima02@ca.com>
2018-08-24 20:39:33 +0530

    added files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+
+ReactDOM.render(
+    <Router basename={process.env.PUBLIC_URL}>
+        <App />
+    </Router>,
+    document.getElementById("root")
+);
+registerServiceWorker();
