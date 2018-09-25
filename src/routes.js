9e1cc78891411c5cb58e785299465fccec92b0a5
Odiveti, Maheswara Reddy <odima02@ca.com>
2018-09-25 21:27:14 +0530

    added

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Login from './components/Login/Login';
-import LandingPage from './components/LandingPage/LandingPage';
 import SearchPage from "./components/SearchPage/SearchPage";
 import { Route } from "react-router-dom";
 
@@ -11,9 +10,6 @@ const createRoutes = () => (
         />
         <Route  exact path="/SearchPage" 
             render={props => ( <SearchPage  {...props} /> )}
-        />
-        <Route  exact path="/LandingPage" 
-            render={props => ( <LandingPage  {...props} /> )}
         />
     </div>
 );
